c6371da341e49506f9467703e0a810fa72230be5
Noran Diaconu <diaconuna@email.appstate.edu>
2015-02-16 14:48:02 -0500

    pipes

--- main.js
+++ main.js
@@ -36,10 +36,10 @@ var mainState = {
         // Create a group of 20 pipes
         this.pipes = game.add.group();
         this.pipes.enableBody = true;
-        this.pipes.createMultiple(30, 'pipe');  
+        this.pipes.createMultiple(10, 'pipe');  
 
         // Timer that calls 'addRowOfPipes' ever 1.5 seconds
-        this.timer = this.game.time.events.loop(1000, this.addRowOfPipes, this);           
+        this.timer = this.game.time.events.loop(1500, this.addRowOfPipes, this);           
 
         // Add a score label on the top left of the screen
         this.score = 0;
