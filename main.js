a5957ba3d7792e4045c64e923a2949172399691f
Noran Diaconu <diaconuna@email.appstate.edu>
2015-02-16 14:27:06 -0500

    Add mouse

--- main.js
+++ main.js
@@ -30,7 +30,9 @@ var mainState = {
 
         // Call the 'jump' function when the spacekey is hit
         var spaceKey = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-        spaceKey.onDown.add(this.jump, this); 
+        spaceKey.onDown.add(this.jump, this);
+        var click = this.game.mouse.button(Phaser.Mouse.LEFT_BUTTON);
+        click.add(this.jump, this); 
 
         // Create a group of 20 pipes
         this.pipes = game.add.group();
