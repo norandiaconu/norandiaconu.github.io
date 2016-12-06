e3641facc1a692b0bdd9d7efee629c7909d7f967
Noran Diaconu <diaconuna@email.appstate.edu>
2016-12-06 16:54:20 -0500

    Resume Button

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 function main() {
-  $('.info').hide();
-  $('.info').fadeIn(1000);
   $('.resume').hide();
+  $('.info').hide();
+  $('.button').hide();
+  $('.info').fadeIn(1000);
+  $('.button').fadeIn(2000);
   $('.button').on('click', function() {
 	$('.info').fadeToggle(200);
     $(this).next().toggle();
